c7a96b86ee88799ec7b5fde538a0ab41ad3f9907
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-07-27 00:32:42 +0530

    Venkatesh-intro-session

--- j-code/Temperature.java
+++ j-code/Temperature.java
@@ -1,5 +1,12 @@
 public class Temperature {
     public static void main(String[] args){
-        System.out.println("Please Enter the which Type of Temperature you want to Convert")
+        System.out.println("Please Enter the which Type of Temperature you want to Convert");
+        System.out.println("For Example");
+        System.out.println("Case One : Celsius to Fahrenheit");
+        System.out.println("Case Two : Celsius to kelvin");
+        System.out.println("Case Three : Fahrenheit to Celsius");
+        System.out.println("Case Four : Fahrenheit to Kelvin");
+        System.out.println("Case Five : Kelvin to Fehrenheit");
+        System.out.println("Case Six : Kelvin to Clesius");
     }
 }
