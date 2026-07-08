3b8fe0fdc33483d40e7e1f85d7eb3e3f10bc0dd6
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-07-08 23:20:28 +0530

    initial-version-file-added

---
+++ src/in/sp/arrays/VowelsAndConsonants.java
@@ -0,0 +1,26 @@
+package in.sp.arrays;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class VowelsAndConsonants {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String name = obj.nextLine().toLowerCase();
+        int vowels = 0;
+        int consonants = 0;
+        for(int i = 0;i<name.length();i++){
+            char ch = name.charAt(i);
+            if(ch >= 'a'&& ch <='z'){
+                if (ch=='a' || ch=='e'|| ch=='i'|| ch=='o'||ch=='u'){
+                    vowels++;
+                }else {
+                    consonants++;
+                }
+            }
+        }
+        System.out.println("Vowels: "+vowels);
+        System.out.println("Consonants: "+consonants);
+    }
+}
