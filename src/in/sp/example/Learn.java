930638c3c446e6a45776bd280f03a8179dc515a1
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-07-10 23:23:32 +0530

    constructor-file-added

---
+++ src/in/sp/example/Learn.java
@@ -0,0 +1,17 @@
+package in.sp.example;
+
+
+class Book{
+    String bName;
+    int bId;
+    Book(String bName, int bId){
+        this.bName=bName;
+        this.bId = bId;
+    }
+}
+public class Learn {
+    public static void main(String[] args) {
+        Book b = new Book("Java",22);
+        System.out.println("The Book Name is: "+b.bName+" and Id is: "+b.bId);
+    }
+}
