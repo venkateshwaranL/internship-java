4662cb348b3754ed29973c7b021925b055a3f2a0
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-07-11 00:56:36 +0530

    initial-added-file

---
+++ src/in/sp/example/Main.java
@@ -0,0 +1,20 @@
+package in.sp.example;
+
+class Books{
+    String name;
+    int pages;
+    String author;
+    int currentPage;
+
+    Books(String name,int pages,String author,int currentPage){
+        this.name = name;
+        this.author=author;
+        this.pages=pages;
+        this.currentPage=currentPage;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
