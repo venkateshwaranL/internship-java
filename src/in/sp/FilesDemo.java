ab20b819170b6e4779c23c4bc67f42ff3bb35fdc
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-07-30 01:11:58 +0530

    file-class-method-createNewFile-for-file-creation

--- src/in/sp/FilesDemo.java
+++ src/in/sp/FilesDemo.java
@@ -1,16 +1,28 @@
 package in.sp;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FilesDemo {
     public static void main(String[] args) {
-        File fl = new File("E:\\Internship\\FilesDemo");
-        boolean exists = fl.exists();
-        System.out.println(exists);
-        if(exists==false){
-            fl.mkdir();
-            exists = fl.exists();
-            System.out.println(exists);
+//        File fl = new File("E:\\Internship\\FilesDemo\\Language\\Java");
+//        boolean exists = fl.exists();
+//        System.out.println(exists);
+//        if(exists==false){
+//            fl.mkdirs();
+//            exists = fl.exists();
+//            System.out.println(exists);
+//        }
+        File fls = new File("E:\\Internship\\FilesDemo\\demo.txt");
+        boolean existsFileName = fls.exists();
+        System.out.println(existsFileName);
+        if (existsFileName == false){
+            try {
+                boolean newFileCreated = fls.createNewFile();
+                System.out.println("New File Created: "+newFileCreated);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 }
