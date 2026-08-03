c3ed8b5494e3637b5a70e5ede5746d233fd26987
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-08-03 23:57:04 +0530

    Frequency-problem

---
+++ src/in/sp/example/Frequency.java
@@ -0,0 +1,21 @@
+package in.sp.example;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Frequency {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String name = obj.nextLine().toLowerCase();
+        int[] frequency = new int[256];
+        for (int i = 0; i < name.length(); i++) {
+            frequency[name.charAt(i)]++;
+        }
+        for (int i = 0; i < 256; i++) {
+            if (frequency[i] > 0){
+                System.out.println((char)i+" = "+frequency[i]);
+            }
+        }
+    }
+}
