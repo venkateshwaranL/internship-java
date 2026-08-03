8f24a923dc9d7680151a7c9813f171b7d4433e0c
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-08-03 23:56:42 +0530

    count-number

---
+++ src/in/sp/example/Count.java
@@ -0,0 +1,13 @@
+package in.sp.example;
+
+public class Count {
+    public static void main(String[] args) {
+        int num = 14645;
+        int count=0;
+        while(num > 0){
+            num = num/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
