3db9cc1d964f16a1016a06e71950b8f7d426b3d6
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-08-04 01:25:25 +0530

    sumofNumber-problem

---
+++ src/in/sp/example/SumofN.java
@@ -0,0 +1,12 @@
+package in.sp.example;
+
+public class SumofN {
+    public static void main(String[] args) {
+        int num = 5;
+        int sum = 0;
+        for(int i=1;i<=num;i++){
+            sum = sum +i;
+        }
+        System.out.println(sum);
+    }
+}
