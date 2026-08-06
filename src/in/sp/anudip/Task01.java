4550080a935a16e2c91e66a9fe60dbf98dd7366b
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-08-07 00:13:56 +0530

    datatype-problem

---
+++ src/in/sp/anudip/Task01.java
@@ -0,0 +1,24 @@
+package in.sp.anudip;
+
+public class Task01 {
+    public static void main(String[] args) {
+        byte a = 12;
+        short b = 56;
+        int c = 2457;
+        long d = 45784578;
+        float e = 457.56f;
+        double f = 74857.45488;
+        char g = 'V';
+        boolean h = true;
+        String name = "Venkateshwaran";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(name);
+    }
+}
