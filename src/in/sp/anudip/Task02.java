d0d7010d995969a67901f1d09a9ec31a116333c5
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-08-07 00:14:23 +0530

    datatype-double-problem

---
+++ src/in/sp/anudip/Task02.java
@@ -0,0 +1,11 @@
+package in.sp.anudip;
+
+public class Task02 {
+    public static void main(String[] args) {
+        double price = 500.0;
+        double gst = 18.0;
+
+        System.out.println("Product Price : ₹" + price);
+        System.out.println("GST Percentage : " + gst + "%");
+    }
+}
