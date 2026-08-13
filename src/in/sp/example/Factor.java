c4ac7c62e6a3a2358100729e24f3c01e6babae52
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-08-13 20:13:59 +0530

    FactorExample-problem

--- src/in/sp/example/Factor.java
+++ src/in/sp/example/Factor.java
@@ -1,17 +1,21 @@
 package in.sp.example;
 
+import java.util.Scanner;
+
 public class Factor {
     public static void main(String[] args) {
-        int num = 36;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        int num = obj.nextInt();
         int i;
-        for(i = 1;i*i<=num;i++){
+        for(i=1;i*i<=num;i++){
             if(num%i==0){
                 System.out.print(i+" ");
             }
         }
-        for(i=i-1;i >=1;i--){
-            if(num%i==0 && i != (num/i)){
-                System.out.print((num / i)+" ");
+        for(i=i-1;i>=1;i--){
+            if(num%i==0 && i!=(num/i)){
+                System.out.print(num/i+" ");
             }
         }
     }
