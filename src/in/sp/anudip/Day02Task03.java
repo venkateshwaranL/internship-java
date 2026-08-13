3b0f2c94283ed1364fa12372780fcabe7937059e
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-08-13 23:15:24 +0530

    anudip-task-evenNumbers

---
+++ src/in/sp/anudip/Day02Task03.java
@@ -0,0 +1,13 @@
+package in.sp.anudip;
+
+public class Day02Task03 {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=50){
+            if (i%2==0){
+                System.out.print(i+" ");
+            }
+            i++;
+        }
+    }
+}
