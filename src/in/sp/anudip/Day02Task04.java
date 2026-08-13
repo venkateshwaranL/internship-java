3f534c05ea543cdd0e3b9868f65eb501544d7fe1
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-08-13 23:19:17 +0530

    anudip-task-factorial

---
+++ src/in/sp/anudip/Day02Task04.java
@@ -0,0 +1,16 @@
+package in.sp.anudip;
+
+import java.util.Scanner;
+
+public class Day02Task04 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the Nunber : ");
+        int num = obj.nextInt();
+        int fact = 1;
+        for (int i=1;i<=num;i++){
+            fact*=i;
+        }
+        System.out.println("Factorial of "+num+" is "+fact);
+    }
+}
