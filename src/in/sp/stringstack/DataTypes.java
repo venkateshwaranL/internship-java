0cd801f0f0e1184f70177900fa2a0d868f19c905
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-08-28 23:54:32 +0530

    stringstack-practice

---
+++ src/in/sp/stringstack/DataTypes.java
@@ -0,0 +1,5 @@
+package in.sp.stringstack;
+
+public class DataTypes {
+
+}
---
+++ src/in/sp/stringstack/LogicalOperators.java
@@ -0,0 +1,12 @@
+package in.sp.stringstack;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+        boolean d = false;
+        System.out.println(a && c);
+        System.out.println(b && d);
+    }
+}
