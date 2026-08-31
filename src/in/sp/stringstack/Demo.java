47032048d7716917848cc03526bfe200b8df076b
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-08-31 22:36:36 +0530

    stringstack-practice

---
+++ src/in/sp/stringstack/Demo.java
@@ -0,0 +1,16 @@
+package in.sp.stringstack;
+
+public class Demo {
+    public static void main(String[] args) {
+        int temp = 1_0;
+        System.out.println(temp);
+        int temp3 = 1______0;
+        System.out.println(temp3);
+        float rn1 = 31.45f;
+        System.out.println(rn1);
+        float rn2 = 3_1.45f;
+        System.out.println(rn2);
+        float rn6 = 31.4_5f;
+        System.out.println(rn6);
+    }
+}
