50d33b7af230e7c5d5f21bcba240de9d38a1c12d
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-09-02 23:18:04 +0530

    stringstack-practice-file-modified

--- src/in/sp/stringstack/Main.java
+++ src/in/sp/stringstack/Main.java
@@ -2,14 +2,8 @@ package in.sp.stringstack;
 
 public class Main {
     static public void main(String[] args) {
-        System.out.println("Venkateshwaran here !");
-        String name = "Ram";
-        int _value = 50;
-        int $age = 45;
-        System.out.println(_value);
-        System.out.println($age);
-        System.out.println(name);
-        int num = 16;
-        System.out.println(num<<2);
+        boolean state1 = true;
+        boolean state2 = false;
+        System.out.println("Result of the true AND false is: "+(state1&&state2));
     }
 }
