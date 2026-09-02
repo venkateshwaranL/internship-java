5aa0a78820321511e0738f801ef4fda9cf98cc89
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2026-09-02 22:43:16 +0530

    stringstack-practice

---
+++ src/in/sp/stringstack/Main.java
@@ -0,0 +1,15 @@
+package in.sp.stringstack;
+
+public class Main {
+    static public void main(String[] args) {
+        System.out.println("Venkateshwaran here !");
+        String name = "Ram";
+        int _value = 50;
+        int $age = 45;
+        System.out.println(_value);
+        System.out.println($age);
+        System.out.println(name);
+        int num = 16;
+        System.out.println(num<<2);
+    }
+}
